10de32344e2875a2d2099b9244c9cc7134fcaa12
Jomin George Attayil <jomyattayil@gmail.com>
2021-05-06 19:48:18 +0530

    Web Designing Challenge
    HTML,CSS,JAVASCRIPT,JQUERY,BOOTSTRAP

---
+++ Calculaor/script.js
@@ -0,0 +1,12 @@
+function buttonclick(val){
+    document.getElementById("screen").value+=val
+
+}
+function clearscrn(){
+    document.getElementById("screen").value=""
+}
+function EqualClick(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
